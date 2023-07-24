178d1aa7d57ba97bd6d974519794b0025c18202e
Yujia Wang <wang.yujia111@gmail.com>
2023-07-24 15:21:48 -0400

    added bead controllers

---
+++ ArtisansBeadStudio/Controllers/BeadController.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using ArtisansBeadStudio.Models;
+using System.Web.Script.Serialization;
+using ArtisansBeadStudio.Models.ViewModels;
+using System.Diagnostics;
+
+namespace ArtisansBeadStudio.Controllers
+{
+    public class BeadController : Controller
+    {
+        //code factoring
+        private static readonly HttpClient client;
+        static BeadController()
+        {
+            HttpClientHandler handler = new HttpClientHandler()
+            {
+                AllowAutoRedirect = false,
+                //cookies are manually set in RequestHeader
+                UseCookies = false
+            };
+
+            client = new HttpClient(handler);
+            client.BaseAddress = new Uri("https://localhost:44383/api/");
+        }
+        private JavaScriptSerializer jss = new JavaScriptSerializer();
+
+
+        /// <summary>
+        /// Grabs the authentication cookie sent to this controller.
+        /// </summary>
+        private void GetApplicationCookie()
+        {
+            string token = "";
+            client.DefaultRequestHeaders.Remove("Cookie");
+            if (!User.Identity.IsAuthenticated) return;
+
+            HttpCookie cookie = System.Web.HttpContext.Current.Request.Cookies.Get(".AspNet.ApplicationCookie");
+            if (cookie != null) token = cookie.Value;
+            Debug.WriteLine("Token Submitted is : " + token);
+            if (token != "") client.DefaultRequestHeaders.Add("Cookie", ".AspNet.ApplicationCookie=" + token);
+
+            return;
+        }
+
+
+
+
+
+        /// <summary>
+        /// Accessing information from bead api controller to get the list of all the beads
+        /// </summary>
+        /// <returns>List of beads</returns>
+        /// url: https://localhost:44386/api/BeadData/ListBeads
+        /// <example>
+        /// GET: Bead/List
+        /// </example>
+        public ActionResult List()
+        {
+            string url = "BeadData/ListBeads";
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            IEnumerable<BeadDto> beads = response.Content.ReadAsAsync<IEnumerable<BeadDto>>().Result;
+
+            return View(beads);
+        }
+
+        /// <summary>
+        /// Accessing information from bead api controller to get the information of a specific bead
+        /// </summary>
+        /// <param name="id">Primary key of a specific bead</param>
+        /// <returns>The information of that specific bead</returns>
+        /// url: https://localhost:44386/api/BeadData/FindBead/{id}
+        ///<example>
+        /// GET: Bead/Details/{id}
+        /// </example>
+        public ActionResult Details(int id)
+        {
+            DetailsBead ViewModels = new DetailsBead();
+
+            string url = "BeadData/FindBead/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            BeadDto specificBead = response.Content.ReadAsAsync<BeadDto>().Result;
+            ViewModels.specificBead = specificBead;
+
+            //show associated keychains with this specific bead
+            url = "KeychainData/ListKeychainsForBead/" + id;
+            response = client.GetAsync(url).Result;
+            IEnumerable<KeychainDto> associatedKeychain = response.Content.ReadAsAsync<IEnumerable<KeychainDto>>().Result;
+            ViewModels.associatedKeychain = associatedKeychain;
+
+            //show aviliable keychains
+            url = "KeychainData/ListKeychainsNotIncludeBead/" + id;
+            response = client.GetAsync(url).Result;
+            IEnumerable<KeychainDto> aviliableKeychain = response.Content.ReadAsAsync<IEnumerable<KeychainDto>>().Result;
+            ViewModels.aviliableKeychain = aviliableKeychain;
+
+
+            return View(ViewModels);
+        }
+
+
+
+        //POST: Bead/Associate/{beadid}
+        [HttpPost]
+        [Authorize]
+        public ActionResult Associate(int id, int keychainid)
+        {
+            //get token
+            GetApplicationCookie();
+            //Debug.WriteLine("beadid: " + id + "keychainid: " + keychainid);
+
+            //call api to aassociate the bead with keychain
+            string url = "BeadData/AssociateBeadsWithKeychain/" + id + "/" + keychainid;
+            HttpContent content = new StringContent("");
+            content.Headers.ContentType.MediaType = "application/json";
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+
+            return RedirectToAction("Details/" + id);
+        }
+
+
+
+
+        //Get: Bead/UnAssociate/{beadid}?KeychainId={keychainid}
+        [HttpGet]
+        [Authorize]
+        public ActionResult UnAssociate(int id, int keychainid)
+        {
+            GetApplicationCookie();
+            //Debug.WriteLine("beadid: " + id + "keychainid: " + keychainid);
+
+            //call api to aassociate the bead with keychain
+            string url = "BeadData/RemoveBeadsWithKeychain/" + id + "/" + keychainid;
+            HttpContent content = new StringContent("");
+            content.Headers.ContentType.MediaType = "application/json";
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+
+            return RedirectToAction("Details/" + id);
+        }
+
+
+
+
+        /// <summary>
+        /// The MVC5 view called New.cshtml has a form to collect data for creating a new bead
+        /// </summary>
+        /// <returns>Send collected data to Create Method</returns>
+        /// GET: Bead/New
+        [Authorize]
+        public ActionResult New()
+        {
+
+            //information about all colours in the system.
+            //GET api/BeadColour/List
+            string url = "BeadColourData/ListBeadColours";
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            IEnumerable<BeadColourDto> beadcoloursOptions = response.Content.ReadAsAsync<IEnumerable<BeadColourDto>>().Result;
+
+            return View(beadcoloursOptions);
+        }
+
+        /// <summary>
+        /// Add a new bead to the system using the API
+        /// </summary>
+        /// <param name="bead">Bead class</param>
+        /// curl -d @bead.json -H "Content-type:application/json" https://localhost:44386/api/BeadData/AddBead   
+        /// <returns>
+        /// Successful: redirect to List page
+        /// Fail: redirect to Error page
+        /// </returns>
+        /// POST: Bead/Create
+        [HttpPost]
+        [Authorize]
+        public ActionResult Create(Bead bead)
+        {
+            GetApplicationCookie();
+            string url = "BeadData/AddBead/";
+            string jsonpayload = jss.Serialize(bead);
+
+            HttpContent content = new StringContent(jsonpayload);
+            content.Headers.ContentType.MediaType = "application/json";
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+
+        }
+
+        /// <summary>
+        /// The MVC5 view called Error.cshtml will hold error message
+        /// </summary>
+        /// GET: Bead/Error
+        public ActionResult Error()
+        {
+            return View();
+        }
+
+
+        /// <summary>
+        /// The MVC5 view called Edit.cshtml has a form to collect the updating data for a sepcific bead.
+        /// </summary>
+        /// <param name="id">The specific bead primary key</param>
+        /// <returns>Send collected data to Update Method</returns>
+        /// GET: Bead/Edit/{id}
+        [Authorize]
+        public ActionResult Edit(int id)
+        {
+
+            UpdateBead ViewModel = new UpdateBead();
+
+            //existing bead information
+            string url = "BeadData/FindBead/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            BeadDto SelectedBead = response.Content.ReadAsAsync<BeadDto>().Result;
+            ViewModel.SelectedBead = SelectedBead;
+
+            //include all the bead colour to choose from when update the bead
+            url = "BeadColourData/ListBeadColours";
+            response = client.GetAsync(url).Result;
+            IEnumerable<BeadColourDto> beadcoloursOptions = response.Content.ReadAsAsync<IEnumerable<BeadColourDto>>().Result;
+            ViewModel.beadcoloursOptions = beadcoloursOptions;
+
+
+
+
+            return View(ViewModel);
+
+        }
+
+        /// <summary>
+        /// Update the information of a specific bead to the system using the API
+        /// </summary>
+        /// <param name="id">The specific bead primary key</param>
+        /// <param name="bead">Bead class</param>
+        /// curl -d @beadupdate.json -H "Content-type:application/json" https://localhost:44386/api/BeadData/UpdateBead/7
+        /// <returns>
+        /// Successful: redirect to List page
+        /// Fail: redirect to Error page
+        /// </returns>
+        /// POST: Bead/Update/{id}
+        [HttpPost]
+        [Authorize]
+        public ActionResult Update(int id, Bead bead, HttpPostedFileBase beadPic)
+        {
+            GetApplicationCookie();
+            string url = "BeadData/UpdateBead/" + id;
+            string jsonpayload = jss.Serialize(bead);
+
+            HttpContent content = new StringContent(jsonpayload);
+            content.Headers.ContentType.MediaType = "application/json";
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+            Debug.WriteLine("Content: " + content);
+
+
+            if (response.IsSuccessStatusCode && beadPic != null)
+            {
+                Debug.WriteLine("working????");
+                url = "BeadData/UploadBeadPic/" + id;
+
+                MultipartFormDataContent requestcontent = new MultipartFormDataContent();
+                HttpContent imagecontent = new StreamContent(beadPic.InputStream);
+                requestcontent.Add(imagecontent, "beadPic", beadPic.FileName);
+                response = client.PostAsync(url, requestcontent).Result;
+
+                return RedirectToAction("Details/" + id);
+            }
+
+            else if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Details/" + id);
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+
+        }
+
+        /// <summary>
+        /// The MVC5 view called DeleteConfirm.cshtml to confirm with user whether they want to delete the selected bead
+        /// </summary>
+        /// <param name="id">The selected bead primary key</param>
+        /// <returns>send the confirm answer to Delete method</returns>
+        /// GET: Bead/Delete/{id}
+        [Authorize]
+        public ActionResult DeleteConfirm(int id)
+        {
+            string url = "BeadData/FindBead/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            BeadDto selectedBead = response.Content.ReadAsAsync<BeadDto>().Result;
+            return View(selectedBead);
+        }
+
+        /// <summary>
+        /// Delete the specific bead
+        /// </summary>
+        /// <param name="id">The selected bead primary key</param>
+        /// <returns>        
+        /// Successful: redirect to List page
+        /// Fail: redirect to Error page
+        /// </returns>
+        /// POST: Bead/Delete/{id}
+        [HttpPost]
+        [Authorize]
+        public ActionResult Delete(int id)
+        {
+            GetApplicationCookie();
+            string url = "BeadData/DeleteBead/" + id;
+
+            HttpContent content = new StringContent("");
+            content.Headers.ContentType.MediaType = "application/json";
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+    }
+}
--- ArtisansBeadStudio/Controllers/BeadDataController.cs
+++ ArtisansBeadStudio/Controllers/BeadDataController.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web;
 using System.Web.Http;
 using System.Web.Http.Description;
 using ArtisansBeadStudio.Models;
+using System.Diagnostics;
 
 namespace ArtisansBeadStudio.Controllers
 {
@@ -16,28 +19,241 @@ namespace ArtisansBeadStudio.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
-        // GET: api/BeadData
-        public IQueryable<Bead> GetBeads()
+        /// <summary>
+        /// Returns all the beads in the system.
+        /// </summary>
+        /// <returns>
+        /// All the beads in the system with the properties of beads and also the colour and colour property of the beads
+        /// </returns>
+        /// <example>
+        /// GET: api/BeadData/ListBeads
+        /// </example>
+        [HttpGet]
+        [ResponseType(typeof(BeadDto))]
+        public IHttpActionResult ListBeads()
         {
-            return db.Beads;
+            List<Bead> Beads = db.Beads.ToList();
+            List<BeadDto> BeadDtos = new List<BeadDto>();
+
+            Beads.ForEach(b => BeadDtos.Add(new BeadDto()
+            {
+                BeadId = b.BeadId,
+                BeadDescription = b.BeadDescription,
+                BeadName = b.BeadName,
+                BeadPicture = b.BeadPicture,
+                ColourId = b.ColourId,
+                ColourName = b.BeadColour.ColourName,
+                ColourProperty = b.BeadColour.ColourProperty
+            }));
+
+            return Ok(BeadDtos);
         }
 
-        // GET: api/BeadData/5
-        [ResponseType(typeof(Bead))]
-        public IHttpActionResult GetBead(int id)
+
+
+        /// <summary>
+        /// Gather information about all the beads related to a particular colour.
+        /// </summary>
+        /// <returns>
+        /// All the beads in the system with the properties of beads and also the colour and colour property of the beads match with the particular colour ID
+        /// </returns>
+        /// <param name="id">Colour ID</param>
+        /// <example>
+        /// GET: api/BeadData/ListBeadsForColour/1
+        /// </example>
+        [HttpGet]
+        [ResponseType(typeof(BeadDto))]
+        public IHttpActionResult ListBeadsForColour(int id)
         {
-            Bead bead = db.Beads.Find(id);
-            if (bead == null)
+            List<Bead> Beads = db.Beads.Where(b => b.ColourId == id).ToList();
+            List<BeadDto> BeadDtos = new List<BeadDto>();
+
+            Beads.ForEach(b => BeadDtos.Add(new BeadDto()
+            {
+                BeadId = b.BeadId,
+                BeadDescription = b.BeadDescription,
+                BeadName = b.BeadName,
+                BeadPicture = b.BeadPicture,
+                ColourId = b.ColourId,
+                ColourName = b.BeadColour.ColourName,
+                ColourProperty = b.BeadColour.ColourProperty
+            }));
+
+            return Ok(BeadDtos);
+        }
+
+
+
+        /// <summary>
+        /// Gather information about all the beads related to a particular keychain.
+        /// </summary>
+        /// <returns>
+        /// All the beads in the system with the properties of beads and also the colour and colour property of the beads that match to a particular keychain ID
+        /// </returns>
+        /// <param name="id">Keychain ID</param>
+        /// <example>
+        /// GET: api/BeadData/ListBeadsForKeychain/1
+        /// </example>
+        [HttpGet]
+        [ResponseType(typeof(BeadDto))]
+        public IHttpActionResult ListBeadsForKeychain(int id)
+        {
+            //all beads that have keychians which match with the ID
+            List<Bead> Beads = db.Beads.Where(
+                b => b.Keychains.Any(
+                    k => k.KeychainId == id
+                )).ToList();
+            List<BeadDto> BeadDtos = new List<BeadDto>();
+
+            Beads.ForEach(b => BeadDtos.Add(new BeadDto()
+            {
+                BeadId = b.BeadId,
+                BeadDescription = b.BeadDescription,
+                BeadName = b.BeadName,
+                BeadPicture = b.BeadPicture,
+                ColourId= b.ColourId,
+                ColourName = b.BeadColour.ColourName,
+                ColourProperty = b.BeadColour.ColourProperty
+            }));
+
+            return Ok(BeadDtos);
+        }
+
+        /// <summary>
+        /// Associate a particular keychain with a particular bead
+        /// </summary>
+        /// <param name="beadid">The bead id primary key</param>
+        /// <param name="keychainid">The keychain id primary key</param>
+        /// <returns>
+        /// HEADER: 200(OK)
+        /// or
+        /// HEADER: 404(NOT FOUND)
+        /// </returns>
+        /// <example>
+        /// POST: api/BeadData/AssociateBeadsWithKeychain/{beadid}/{keychainid}
+        /// curl -d "" -v https://localhost:44386/api/BeadData/AssociateBeadsWithKeychain/3/4
+        /// </example>
+        [HttpPost]
+        [Route("api/BeadData/AssociateBeadsWithKeychain/{beadid}/{keychainid}")]
+        [Authorize]
+        public IHttpActionResult AssociateBeadsWithKeychain(int beadid, int keychainid)
+        {
+            Bead SelectedBead = db.Beads.Include(b => b.Keychains).Where(b => b.BeadId == beadid).FirstOrDefault();
+            Keychain SelectedKeychain = db.Keychains.Find(keychainid);
+
+            if (SelectedBead == null || SelectedKeychain == null)
+            {
+                return NotFound();
+            };
+
+            SelectedBead.Keychains.Add(SelectedKeychain);
+            db.SaveChanges();
+
+            return Ok();
+        }
+
+
+
+
+        /// <summary>
+        /// remove an association between a particular keychain and a particular bead
+        /// </summary>
+        /// <param name="beadid">The bead id primary key</param>
+        /// <param name="keychainid">The keychain id primary key</param>
+        /// <returns>
+        /// HEADER: 200(OK)
+        /// or
+        /// HEADER: 404(NOT FOUND)
+        /// </returns>
+        /// <example>
+        /// POST: api/BeadData/RemoveBeadsWithKeychain/{beadid}/{keychainid}
+        /// curl -d "" -v https://localhost:44386/api/BeadData/RemoveBeadsWithKeychain/3/4
+        /// </example>
+        [HttpPost]
+        [Route("api/BeadData/RemoveBeadsWithKeychain/{beadid}/{keychainid}")]
+        [Authorize]
+        public IHttpActionResult RemoveBeadsWithKeychain(int beadid, int keychainid)
+        {
+            Bead SelectedBead = db.Beads.Include(b => b.Keychains).Where(b => b.BeadId == beadid).FirstOrDefault();
+            Keychain SelectedKeychain = db.Keychains.Find(keychainid);
+
+            if (SelectedBead == null || SelectedKeychain == null)
+            {
+                return NotFound();
+            };
+
+            SelectedBead.Keychains.Remove(SelectedKeychain);
+            db.SaveChanges();
+
+            return Ok();
+        }
+
+
+        /// <summary>
+        /// Return the properties of one specific chosen bead in the system
+        /// </summary>
+        /// <param name="id">The BeadId of that chosen bead</param>
+        /// <returns>
+        /// All the properties releated to that chosen bead, including the bead description, bead name, bead picture(name of the picture), bead colour and the colour property
+        /// </returns>
+        /// <example>
+        /// GET: api/BeadData/FindBead/3
+        /// </example> 
+        [HttpGet]
+        [ResponseType(typeof(Bead))]
+        public IHttpActionResult FindBead(int id)
+        {
+            Bead Bead = db.Beads.Find(id);
+            BeadDto BeadDto = new BeadDto()
+            {
+                BeadId = Bead.BeadId,
+                BeadDescription = Bead.BeadDescription,
+                BeadName = Bead.BeadName,
+                BeadPicture = Bead.BeadPicture,
+                ColourId= Bead.ColourId,
+                ColourName = Bead.BeadColour.ColourName,
+                ColourProperty = Bead.BeadColour.ColourProperty
+            };
+
+            if (Bead == null)
             {
                 return NotFound();
             }
 
-            return Ok(bead);
+            return Ok(BeadDto);
         }
 
-        // PUT: api/BeadData/5
+
+        /// <summary>
+        /// Update a specific bead in the system with POST Data input
+        /// </summary>
+        /// <param name="id">The primary key of the specific bead</param>
+        /// <param name="bead">JSON FORM DATA of a bead</param>
+        /// <returns>
+        /// HEADER: 204 (Success, No Content Response)
+        /// or
+        /// HEADER: 400 (Bad Request)
+        /// or
+        /// HEADER: 404 (Not Found)
+        /// </returns>
+        /// <example>
+        /// POST: api/BeadData/UpdateBead/5
+        /// </example>
+        /// <example>
+        /// CREATE: A beadupdate.json file, with JSON FORM DATA that want to input into system
+        ///        {
+        ///             "BeadId": 7,
+        ///             "BeadName": "Pink blue bead with cat",
+        ///             "BeadDescription": "Pink bead with cat face and fishes around it",
+        ///             "BeadPicture": " ",
+        ///             "ColourId": 1
+        ///         }
+        /// COMMAND WINDOW: curl -d @beadupdate.json -H "Content-type:application/json" https://localhost:44386/api/BeadData/UpdateBead/7
+        /// </example>
         [ResponseType(typeof(void))]
-        public IHttpActionResult PutBead(int id, Bead bead)
+        [HttpPost]
+        [Authorize]
+        public IHttpActionResult UpdateBead(int id, Bead bead)
         {
             if (!ModelState.IsValid)
             {
@@ -50,6 +266,7 @@ namespace ArtisansBeadStudio.Controllers
             }
 
             db.Entry(bead).State = EntityState.Modified;
+            db.Entry(bead).Property(b => b.BeadPicture).IsModified = false;
 
             try
             {
@@ -70,9 +287,95 @@ namespace ArtisansBeadStudio.Controllers
             return StatusCode(HttpStatusCode.NoContent);
         }
 
-        // POST: api/BeadData
+
+        /// <summary>
+        /// receives bead picture data and upload it to the webserver and updates the path in database
+        /// </summary>
+        /// <param name="id">the bead id</param>
+        /// <returns>status code 200 if successful.</returns>
+        /// <example>
+        /// curl -F beadPic=@file.jpg "https://localhost:44386/api/BeadData/UploadBeadPic/4"
+        /// HEADER: enctype=multipart/form-data
+        /// FORM-DATA: image
+        /// </example>
+        /// POST: api/BeadData/UploadBeadPic/{id}
+        [HttpPost]
+        public IHttpActionResult UploadBeadPic(int id)
+        {
+            string picroot;
+            if (Request.Content.IsMimeMultipartContent())
+            {
+                int numfiles = HttpContext.Current.Request.Files.Count;
+
+                if (numfiles == 1 && HttpContext.Current.Request.Files[0] != null)
+                {
+                    var beadPic = HttpContext.Current.Request.Files[0];
+                    if (beadPic.ContentLength > 0)
+                    {
+                        var valtypes = new[] { "jpeg", "jpg", "png", "gif" };
+                        var extension = Path.GetExtension(beadPic.FileName).Substring(1);
+
+                        if (valtypes.Contains(extension))
+                        {
+                            try
+                            {
+                                string fn = id + "." + extension;
+
+                                string path = Path.Combine(HttpContext.Current.Server.MapPath("~/Content/img/beads/"), fn);
+                                beadPic.SaveAs(path);
+                                picroot = fn;
+
+                                Bead Selectedbead = db.Beads.Find(id);
+                                Selectedbead.BeadPicture = picroot;
+                                db.Entry(Selectedbead).State = EntityState.Modified;
+
+                                db.SaveChanges();
+                            }
+                            catch (Exception ex)
+                            {
+                                return BadRequest();
+                            }
+                        }
+                    }
+                }
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+
+
+        /// <summary>
+        /// Add a bead in the system
+        /// </summary>
+        /// <param name="bead">JSON FORM DATA of a bead</param>
+        /// <returns>
+        /// Header: 201(Created)
+        /// Content: BeadId, Bead Data 
+        /// or
+        /// Header: 400 (Bad Request)
+        /// </returns>
+        /// <example>
+        /// POST: api/BeadData/AddBead
+        /// </example>
+        /// <example>
+        /// CREATE: A bead.json file, with JSON FORM DATA that want to input into system
+        ///        {
+        ///             "BeadName": "Baby blue bead with cat",
+        ///             "BeadDescription": "Baby blue bead with cat face and fishes around it",
+        ///             "BeadPicture": " ",
+        ///             "ColourId": 3
+        ///         }
+        /// COMMAND WINDOW: curl -d @bead.json -H "Content-type:application/json" https://localhost:44386/api/BeadData/AddBead        
+        /// RETURN:{"BeadId":7,"BeadName":"Baby blue bead with cat","BeadDescription":"Baby blue bead with cat face and fishes around it","BeadPicture":" ","ColourId":3,"BeadColour":null,"Keychains":null}
+        /// </example>
+        [HttpPost]
         [ResponseType(typeof(Bead))]
-        public IHttpActionResult PostBead(Bead bead)
+        [Authorize]
+        public IHttpActionResult AddBead(Bead bead)
         {
             if (!ModelState.IsValid)
             {
@@ -85,8 +388,24 @@ namespace ArtisansBeadStudio.Controllers
             return CreatedAtRoute("DefaultApi", new { id = bead.BeadId }, bead);
         }
 
-        // DELETE: api/BeadData/5
+
+        /// <summary>
+        /// Delete one specific bead from the system by it's ID
+        /// </summary>
+        /// <param name="id">The primary key of the bead that you want to delete</param>
+        /// <returns>
+        /// Header: 200 (OK, if it deletes successfully)
+        /// or
+        /// Header: 404 (NOT FOUND, if it deletes NOT successfully)
+        /// </returns>
+        /// <example>
+        /// Post: api/BeadData/DeleteBead/5
+        /// curl -d "" "https://localhost:44386/api/BeadData/DeleteBead/5"
+        /// Form data: (empty)
+        /// </example>
+        [HttpPost]
         [ResponseType(typeof(Bead))]
+        [Authorize]
         public IHttpActionResult DeleteBead(int id)
         {
             Bead bead = db.Beads.Find(id);
@@ -98,7 +417,7 @@ namespace ArtisansBeadStudio.Controllers
             db.Beads.Remove(bead);
             db.SaveChanges();
 
-            return Ok(bead);
+            return Ok();
         }
 
         protected override void Dispose(bool disposing)
