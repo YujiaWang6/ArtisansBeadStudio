ecc2d860cc30888cb4426f0c72333a0e163e0eac
Yujia Wang <wang.yujia111@gmail.com>
2023-07-24 15:52:29 -0400

    added bead Colour controllers

---
+++ ArtisansBeadStudio/Controllers/BeadColourController.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using ArtisansBeadStudio.Models;
+using System.Web.Script.Serialization;
+using System.Diagnostics;
+using ArtisansBeadStudio.Models.ViewModels;
+
+namespace ArtisansBeadStudio.Controllers
+{
+    public class BeadColourController : Controller
+    {
+        //code factoring
+        private static readonly HttpClient client;
+        static BeadColourController()
+        {
+            HttpClientHandler handler = new HttpClientHandler()
+            {
+                AllowAutoRedirect = false,
+                UseCookies = false
+            };
+
+            client = new HttpClient(handler);
+            client.BaseAddress = new Uri("https://localhost:44383/api/");
+        }
+        private JavaScriptSerializer jss = new JavaScriptSerializer();
+
+
+        /// <summary>
+        /// Grabs the authentication cookie sent to this controller.
+        /// </summary>
+        private void GetApplicationCookie()
+        {
+            string token = "";
+            client.DefaultRequestHeaders.Remove("Cookie");
+            if (!User.Identity.IsAuthenticated) return;
+
+            HttpCookie cookie = System.Web.HttpContext.Current.Request.Cookies.Get(".AspNet.ApplicationCookie");
+            if (cookie != null) token = cookie.Value;
+
+            if (token != "") client.DefaultRequestHeaders.Add("Cookie", ".AspNet.ApplicationCookie=" + token);
+
+            return;
+        }
+
+
+
+        /// <summary>
+        /// Accessing information from beadcolour api controller to get the list of all the bead colours
+        /// </summary>
+        /// <returns>List of bead colours</returns>
+        /// curl: https://localhost:44386/api/BeadColourData/ListBeadColours
+        /// <example>
+        /// GET: BeadColour/List
+        /// </example>
+        public ActionResult List()
+        {
+            string url = "BeadColourData/ListBeadColours";
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            IEnumerable<BeadColourDto> beadcolours = response.Content.ReadAsAsync<IEnumerable<BeadColourDto>>().Result;
+
+            return View(beadcolours);
+        }
+
+
+        /// <summary>
+        /// Accessing information from beadColour api controller to get the information of a specific bead colour
+        /// </summary>
+        /// <param name="id">Primary key of a specific bead colour</param>
+        /// <returns>The information of that specific bead colour</returns>
+        /// curl: https://localhost:44386/api/BeadColourData/FindBeadColour/{id}
+        ///<example>
+        /// GET: BeadColour/Details/{id}
+        /// </example>
+        public ActionResult Details(int id)
+        {
+            DetailsBeadColour ViewModels = new DetailsBeadColour();
+
+            string url = "BeadColourData/FindBeadColour/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            BeadColourDto specificColour = response.Content.ReadAsAsync<BeadColourDto>().Result;
+            ViewModels.specificColour = specificColour;
+
+            //showcase information about beads related to this colour
+            //Send a request to gather information about beads related to a particular colour ID
+            url = "BeadData/ListBeadsForColour/" + id;
+            response = client.GetAsync(url).Result;
+            IEnumerable<BeadDto> relatedBeads = response.Content.ReadAsAsync<IEnumerable<BeadDto>>().Result;
+            ViewModels.relatedBeads = relatedBeads;
+
+            return View(ViewModels);
+        }
+
+
+        /// <summary>
+        /// The MVC5 view called New.cshtml has a form to collect data for creating a new bead colour
+        /// </summary>
+        /// <returns>Send collected data to Create Method</returns>
+        /// GET: BeadColour/New
+        [Authorize]
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Add a new bead colour to the system using the API
+        /// </summary>
+        /// <param name="beadColour">BeadColour class</param>
+        /// curl -d @beadcolour.json -H "Content-type:application/json" https://localhost:44386/api/BeadColourData/AddBeadColour  
+        /// <returns>
+        /// Successful: redirect to List page
+        /// Fail: redirect to Error page
+        /// </returns>
+        /// POST: BeadColour/Create
+        [HttpPost]
+        [Authorize]
+        public ActionResult Create(BeadColour beadColour)
+        {
+
+            GetApplicationCookie();
+            string url = "BeadColourData/AddBeadColour";
+            string jsonpayload = jss.Serialize(beadColour);
+
+            HttpContent content = new StringContent(jsonpayload);
+            content.Headers.ContentType.MediaType = "application/json";
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+
+        }
+
+        /// <summary>
+        /// The MVC5 view called Error.cshtml will hold error message
+        /// </summary>
+        /// GET: BeadColour/Error
+        public ActionResult Error()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// The MVC5 view called Edit.cshtml has a form to collect the updating data for a sepcific bead colour.
+        /// </summary>
+        /// <param name="id">The specific bead colour primary key</param>
+        /// <returns>Send collected data to Update Method</returns>
+        /// GET: BeadColour/Edit/{id}
+        [Authorize]
+        public ActionResult Edit(int id)
+        {
+            string url = "BeadColourData/FindBeadColour/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            BeadColourDto specificColour = response.Content.ReadAsAsync<BeadColourDto>().Result;
+            return View(specificColour);
+        }
+
+
+        /// <summary>
+        /// Update the information of a specific bead colour to the system using the API
+        /// </summary>
+        /// <param name="id">The specific bead colour primary key</param>
+        /// <param name="beadColour">BeadColour class</param>
+        /// curl -d @beadcolourupdate.json -H "Content-type:application/json" https://localhost:44386/api/BeadColourData/UpdateBeadColour/3
+        /// <returns>
+        /// Successful: redirect to List page
+        /// Fail: redirect to Error page
+        /// </returns>
+        /// POST: BeadColour/Update/{id}
+        [HttpPost]
+        [Authorize]
+        public ActionResult Update(int id, BeadColour beadColour)
+        {
+            GetApplicationCookie();
+            string url = "BeadColourData/UpdateBeadColour/" + id;
+            string jsonpayload = jss.Serialize(beadColour);
+
+            HttpContent content = new StringContent(jsonpayload);
+            content.Headers.ContentType.MediaType = "application/json";
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+
+        /// <summary>
+        /// The MVC5 view called DeleteConfirm.cshtml to confirm with user whether they want to delete the selected bead colour
+        /// </summary>
+        /// <param name="id">The selected bead colour primary key</param>
+        /// <returns>send the confirm answer to Delete method</returns>
+        /// GET: BeadColour/Delete/{id}
+        [Authorize]
+        public ActionResult DeleteConfirm(int id)
+        {
+            string url = "BeadColourData/FindBeadColour/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            BeadColourDto selectedBeadColour = response.Content.ReadAsAsync<BeadColourDto>().Result;
+            return View(selectedBeadColour);
+        }
+
+
+        /// <summary>
+        /// Delete the specific bead colour
+        /// </summary>
+        /// <param name="id">The selected bead colour primary key</param>
+        /// <returns>        
+        /// Successful: redirect to List page
+        /// Fail: redirect to Error page
+        /// </returns>
+        /// POST: BeadColour/Delete/{id}
+        [HttpPost]
+        [Authorize]
+        public ActionResult Delete(int id)
+        {
+            GetApplicationCookie();
+            string url = "BeadColourData/DeleteBeadColour/" + id;
+
+            HttpContent content = new StringContent("");
+            content.Headers.ContentType.MediaType = "application/json";
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+    }
+}
---
+++ ArtisansBeadStudio/Controllers/KeychainController.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using ArtisansBeadStudio.Models;
+using System.Web.Script.Serialization;
+using ArtisansBeadStudio.Models.ViewModels;
+using System.Diagnostics;
+
+namespace ArtisansBeadStudio.Controllers
+{
+    public class KeychainController : Controller
+    {
+
+        //code factoring
+        private static readonly HttpClient client;
+        static KeychainController()
+        {
+            HttpClientHandler handler = new HttpClientHandler()
+            {
+                AllowAutoRedirect = false,
+                UseCookies = false
+            };
+
+            client = new HttpClient(handler);
+            client.BaseAddress = new Uri("https://localhost:44383/api/");
+        }
+        private JavaScriptSerializer jss = new JavaScriptSerializer();
+
+
+
+
+        /// <summary>
+        /// Grabs the authentication cookie sent to this controller.
+        /// </summary>
+        private void GetApplicationCookie()
+        {
+            string token = "";
+            client.DefaultRequestHeaders.Remove("Cookie");
+            if (!User.Identity.IsAuthenticated) return;
+
+            HttpCookie cookie = System.Web.HttpContext.Current.Request.Cookies.Get(".AspNet.ApplicationCookie");
+            if (cookie != null) token = cookie.Value;
+
+            if (token != "") client.DefaultRequestHeaders.Add("Cookie", ".AspNet.ApplicationCookie=" + token);
+
+            return;
+        }
+
+
+        /// <summary>
+        /// Accessing information from keychain api controller to get the list of all the keychains
+        /// </summary>
+        /// <returns>List of keychains</returns>
+        /// url: https://localhost:44386/api/KeychainData/ListKeychains
+        /// <example>
+        /// GET: Keychain/List
+        /// </example>
+        public ActionResult List()
+        {
+            string url = "KeychainData/ListKeychains";
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            IEnumerable<KeychainDto> keychains = response.Content.ReadAsAsync<IEnumerable<KeychainDto>>().Result;
+
+            return View(keychains);
+        }
+
+        /// <summary>
+        /// Accessing information from bead api controller to get the information of a specific keychain
+        /// </summary>
+        /// <param name="id">Primary key of a specific keychain</param>
+        /// <returns>The information of that specific keychain</returns>
+        /// url: https://localhost:44386/api/KeychainData/FindKeychain/{id}
+        ///<example>
+        /// GET: Keychain/Details/{id}
+        /// </example>
+        public ActionResult Details(int id)
+        {
+            DetailsKeychain ViewModels = new DetailsKeychain();
+
+            string url = "KeychainData/FindKeychain/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            KeychainDto specificKeychain = response.Content.ReadAsAsync<KeychainDto>().Result;
+            ViewModels.specificKeychain = specificKeychain;
+
+
+            //also show all the beads under this keychian
+            url = "BeadData/ListBeadsForKeychain/" + id;
+            response = client.GetAsync(url).Result;
+            IEnumerable<BeadDto> beadsInKeychain = response.Content.ReadAsAsync<IEnumerable<BeadDto>>().Result;
+            ViewModels.beadsInKeychain = beadsInKeychain;
+
+            return View(ViewModels);
+        }
+
+
+
+        /// <summary>
+        /// The MVC5 view called New.cshtml has a form to collect data for creating a new keychain
+        /// </summary>
+        /// <returns>Send collected data to Create Method</returns>
+        /// GET: Keychain/Create
+        [Authorize]
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Add a new keychain to the system using the API
+        /// </summary>
+        /// <param name="keychain">Keychain class</param>
+        /// curl -d @keychain.json -H "Content-type:application/json" https://localhost:44386/api/KeychainData/AddKeychain    
+        /// <returns>
+        /// Successful: redirect to List page
+        /// Fail: redirect to Error page
+        /// </returns>
+        /// POST: Keychain/Create
+        [HttpPost]
+        [Authorize]
+        public ActionResult Create(Keychain keychain)
+        {
+            GetApplicationCookie();
+            string url = "KeychainData/AddKeychain";
+            string jsonpayload = jss.Serialize(keychain);
+
+            HttpContent content = new StringContent(jsonpayload);
+            content.Headers.ContentType.MediaType = "application/json";
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+
+        /// <summary>
+        /// The MVC5 view called Error.cshtml will hold error message
+        /// </summary>
+        /// GET: Keychain/Error
+        public ActionResult Error()
+        {
+            return View();
+        }
+
+
+        /// <summary>
+        /// The MVC5 view called Edit.cshtml has a form to collect the updating data for a sepcific keychain.
+        /// </summary>
+        /// <param name="id">The specific keychain primary key</param>
+        /// <returns>Send collected data to Update Method</returns>
+        /// GET: Keychain/Edit/{id}
+        [Authorize]
+        public ActionResult Edit(int id)
+        {
+            string url = "KeychainData/FindKeychain/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            KeychainDto specificKeychain = response.Content.ReadAsAsync<KeychainDto>().Result;
+            return View(specificKeychain);
+        }
+
+        /// <summary>
+        /// Update the information of a specific keychain to the system using the API
+        /// </summary>
+        /// <param name="id">The specific keychain primary key</param>
+        /// <param name="bead">Keychain class</param>
+        /// curl -d @keychainupdate.json -H "Content-type:application/json" https://localhost:44386/api/KeychainData/UpdateKeychain/3
+        /// <returns>
+        /// Successful: redirect to List page
+        /// Fail: redirect to Error page
+        /// </returns>
+        /// POST: Keychain/Update/{id}
+        [HttpPost]
+        [Authorize]
+        public ActionResult Update(int id, Keychain keychain)
+        {
+            GetApplicationCookie();
+            string url = "KeychainData/UpdateKeychain/" + id;
+            string jsonpayload = jss.Serialize(keychain);
+
+            HttpContent content = new StringContent(jsonpayload);
+            content.Headers.ContentType.MediaType = "application/json";
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+
+        /// <summary>
+        /// The MVC5 view called DeleteConfirm.cshtml to confirm with user whether they want to delete the selected keychain
+        /// </summary>
+        /// <param name="id">The selected keychain primary key</param>
+        /// <returns>send the confirm answer to Delete method</returns>
+        /// GET: Keychain/Delete/{id}
+        [Authorize]
+        public ActionResult DeleteConfirm(int id)
+        {
+            string url = "KeychainData/FindKeychain/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            KeychainDto selectedKeychain = response.Content.ReadAsAsync<KeychainDto>().Result;
+            return View(selectedKeychain);
+        }
+
+
+
+        /// <summary>
+        /// Delete the specific keychain
+        /// </summary>
+        /// <param name="id">The selected keychain primary key</param>
+        /// <returns>        
+        /// Successful: redirect to List page
+        /// Fail: redirect to Error page
+        /// </returns>
+        /// POST: Keychain/Delete/{id}
+        [HttpPost]
+        [Authorize]
+        public ActionResult Delete(int id)
+        {
+            GetApplicationCookie();
+            string url = "KeychainData/DeleteKeychain/" + id;
+
+            HttpContent content = new StringContent("");
+            content.Headers.ContentType.MediaType = "application/json";
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+    }
+}
